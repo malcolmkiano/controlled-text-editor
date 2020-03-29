30991ec09ec4a4aa117011a43079990fc8f70bdc
Malcolm Kiano <malcolmkiano@gmail.com>
2020-03-29 07:00:02 -0400

    Updates

---
+++ precache-manifest.3aee462f543a50be8035fff4061ea493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569190c06f1fc99a81a1d65c51ec1009",
+    "url": "/controlled-text-editor/index.html"
+  },
+  {
+    "revision": "d9cda3ae87cef5bc6b0b",
+    "url": "/controlled-text-editor/static/css/main.542e27dc.chunk.css"
+  },
+  {
+    "revision": "305c06e50ba4357de873",
+    "url": "/controlled-text-editor/static/js/2.e17571dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/controlled-text-editor/static/js/2.e17571dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9cda3ae87cef5bc6b0b",
+    "url": "/controlled-text-editor/static/js/main.e86fc85e.chunk.js"
+  },
+  {
+    "revision": "167bf769c3520034e273",
+    "url": "/controlled-text-editor/static/js/runtime-main.6fa8f408.js"
+  }
+]);
